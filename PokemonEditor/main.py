25309e91536bceaafbbd7569dce9e6bc1d916f79
KotiSek <otpakali132@gmail.com>
2026-04-25 13:03:23 +0200

    Update main.py

--- PokemonEditor/main.py
+++ PokemonEditor/main.py
@@ -1,8 +1,7 @@
 import sys
 from pathlib import Path
 
-# Upewniamy się, że katalog projektu jest w sys.path
-# (pozwala uruchamiać skrypt spoza jego katalogu)
+
 sys.path.insert(0, str(Path(__file__).parent))
 
 from gui.app import App
